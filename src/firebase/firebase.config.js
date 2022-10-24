237a65d1230aeb1a87d2c1dae01ce94aac2071ee
Saima <saimasawrin324@gmail.com>
2022-10-25 00:44:02 +0600

    Header added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDKshG36R288Q_OFs_hOWXsNklyCMBLEU",
+  authDomain: "learning-platform-cdca3.firebaseapp.com",
+  projectId: "learning-platform-cdca3",
+  storageBucket: "learning-platform-cdca3.appspot.com",
+  messagingSenderId: "787922686589",
+  appId: "1:787922686589:web:da8464d837ba599d1ed714"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
