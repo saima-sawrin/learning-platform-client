e894dc2cf1c740c9567f8602dac7480e77935d75
Saima <saimasawrin324@gmail.com>
2022-10-25 01:05:10 +0600

    router set

--- src/Component/Shared/Header/Header.js
+++ src/Component/Shared/Header/Header.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <div className='mb-3'>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand><Link to='/'>eduLife</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link >All News</Nav.Link>
+          <Nav.Link >Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item >Action</NavDropdown.Item>
+            <NavDropdown.Item >
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item >Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item >
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        {/* <Nav>
+          <>
+            {
+              user?.uid ?
+              <>
+                  <span>{user?.displayName}</span>
+                  <Button variant="light" onClick={handleLogOut}>Log out</Button>
+              </>
+              :
+              <>
+                  <Link className='text-decoration-none text-white ms-2' to='/login'>Login</Link>
+                  <Link className='text-decoration-none text-white ms-2' to='/register'>Register</Link>
+              </>
+          }
+          </>
+          <Nav.Link eventKey={2} >
+          {user?.photoURL?
+                          
+                              <Image
+                                  style={{ height: '30px' }}
+                                  roundedCircle
+                                  src={user.photoURL}>
+                              </Image>
+                       
+                              : <FaUser></FaUser>
+                          }
+          </Nav.Link>
+        </Nav> */}
+        <div className='d-lg-none'>
+  
+   </div>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+   
+      </div>
     );
 };
 
--- src/Route/Route.js
+++ src/Route/Route.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
 
-const Route = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default Route;
+    const router = createBrowserRouter([
+      {
+        path: '/',
+        element: <Main></Main>,
+        children:[
+            {
+                
+            }
+        ]
+      }
+    ])
+
+
+export default router;
